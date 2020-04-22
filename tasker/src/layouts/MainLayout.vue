bacabd3f06f069080e37da38d352005d0c1a4a76
Ogtay Huseynov <ogtayhuseynov0@gmail.com>
2020-04-22 16:25:48 +0400

    a

---
+++ tasker/src/layouts/MainLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-layout view="hHh lpR fff">
+
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
+
+        <q-toolbar-title>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="left" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      left: false
+    }
+  }
+}
+</script>
