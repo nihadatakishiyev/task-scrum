096b1e34ee8d5f0ef509658d3945639559777f1d
niko <niko@n.com>
2020-04-23 19:36:15 +0400

    resource files updated for int

---
+++ backend/database/migrations/2020_04_23_000007_create_user_permissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('project_id')->references('id')->on('projects');
+            $table->integer('permission_type_id')->default(2);
+//            $table->foreignId('permission_type_id')->references('id')->on('list_permissions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_permissions');
+    }
+}
