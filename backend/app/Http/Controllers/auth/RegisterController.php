e14c874e28d7da17ae8a684bb8f02a4c4a5d3d5c
niko <niko@n.com>
2020-04-25 15:35:37 +0400

    reg controller changed

--- backend/app/Http/Controllers/auth/RegisterController.php
+++ backend/app/Http/Controllers/auth/RegisterController.php
@@ -8,15 +8,24 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
     public function register( Request $request) {
-        $validated = $request->validate([
-           'name' => 'required|max:55',
-           'email' => 'email|required|unique:users',
-           'password' => 'required'
+//        $validated = $request->validate([
+//           'name' => 'required|max:55',
+//           'email' => 'email|required|unique:users',
+//           'password' => 'required'
+//        ]);
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required'
         ]);
+        if ($validator->fails()) {
+            return response(['message' => implode("<br/>",$validator->messages()->all())], 422);
+        }
 
         $request->merge(['password' => bcrypt($request->password)]);
 
